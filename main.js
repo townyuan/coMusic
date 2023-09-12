28caaa132e733800393d218a89b1f7d946c7ed12
judy123 <judyab95@yahoo.com.tw>
2023-09-12 19:38:37 +0800

    [modify]search

--- main.js
+++ main.js
@@ -1,22 +1,57 @@
-import 'swiper/css/bundle';
+// Import
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle'; // 覆蓋 bootstrap
+
 import './assets/scss/all.scss';
 import './node_modules/bootstrap/dist/js/bootstrap.min.js';
-import Swiper from 'swiper/bundle';
+
+import AOS from 'aos';
+import './node_modules/aos/dist/aos.css';
+
+import jQuery from 'jquery';
+window.$ = jQuery;
 
 
-// search Swiper:
-const searchSwiper = new Swiper(".searchSwiper", {
+
+// JavaScript
+// News swiper
+const newsSwiper = new Swiper(".newSwiper", {
   slidesPerView: 1,
   spaceBetween: 24,
   loop: true,
+  centeredSlides: true,
 
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    clickable: true,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+})
+
+
+// Comment swiper
+const commentSwiper = new Swiper(".commentSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  loop: true,
+  
   breakpoints: {
     768: {
       slidesPerView: 2,
       spaceBetween: 24,
     },
     992: {
-      slidesPerView: 4,
+      slidesPerView: 3,
       spaceBetween: 24,
     }
   },
@@ -27,12 +62,33 @@ const searchSwiper = new Swiper(".searchSwiper", {
   },
 
   navigation: {
-    nextEl: ".swiper-btn-next",
-    prevEl: ".swiper-btn-prev",
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    clickable: true,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
     clickable: true,
   },
 });
 
+
+// aos
+AOS.init();
+
+
+// jQuery
+// Signin/ login
+$('.needs-validation').each(function(index) {
+  $(this).on('submit', function(e) {
+    e.preventDefault();
+    $(this).addClass('was-validated');
+  });
+});
+
+
+// 
 //scrollbar
 const hitSwiper = new Swiper(".hitSwiper", {
   spaceBetween: 24,
@@ -44,3 +100,4 @@ const hitSwiper = new Swiper(".hitSwiper", {
   },
   slidesPerView: "auto",
 });
+
