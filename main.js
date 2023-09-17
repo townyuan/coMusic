dd92b28252fb48cf900ea6ce0e9bcc67566e585a
Tami1118 <chenziyi93326@gmail.com>
2023-09-17 10:01:50 +0800

    [update] search swiper

--- main.js
+++ main.js
@@ -164,50 +164,51 @@ const swFour = new Swiper(".swFour", {
 });
 
 // Search swiper
-// const searchSwiper = new Swiper(".searchSwiper", {
-//   slidesPerView: 1,
-//   spaceBetween: 24,
-//   // loop: true,
+const searchSwiper = new Swiper(".searchSwiper", {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  loop: true,
 
-//   breakpoints: {
-//     768: {
-//       slidesPerView: 2,
-//       spaceBetween: 24,
-//     },
-//     992: {
-//       slidesPerView: 4,
-//       spaceBetween: 24,
-//     }
-//   },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    992: {
+      slidesPerView: 4,
+      spaceBetween: 24,
+    }
+  },
 
-//   autoplay: {
-//     delay: 2500,
-//     disableOnInteraction: false,
-//   },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
 
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//     clickable: true,
-//   },
+  navigation: {
+    nextEl: ".swiper-button-next-search",
+    prevEl: ".swiper-button-prev-search",
+    clickable: true,
+  },
 
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-// });
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+});
 
 //scrollbar
-// const hitSwiper = new Swiper(".hitSwiper", {
-//   spaceBetween: 24,
-//   //滑鼠中間滾輪可滑動
-//   mousewheel: true,
-//   scrollbar: {
-//     el: ".swiper-scrollbar",
-//     draggable: true
-//   },
-//   slidesPerView: "auto",
-// });
+const hitSwiper = new Swiper(".hitSwiper", {
+  spaceBetween: 24,
+  //滑鼠中間滾輪可滑動
+  mousewheel: true,
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true
+  },
+  slidesPerView: "auto",
+});
+
 
 
 
